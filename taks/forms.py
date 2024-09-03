c30aeee821be952b73c2c49aebc431bc4af25766
koji17x <jandrydavid24@gmail.com>
2024-09-02 22:54:26 -0500

    mejoras 4

--- taks/forms.py
+++ taks/forms.py
@@ -3,11 +3,15 @@ from .models import Task
 
 class taskform(forms.ModelForm):
     class Meta:
-        model= Task
+        model = Task
         fields = ["title", "description", "important"]
-        widgets={
-            "title":forms.TextInput(attrs={"class":"form-control", "placeholder": "Write a title"}),
-            "description":forms.Textarea(attrs={"class":"form-control", "placeholder": "Write a description"}),
-            "important":forms.CheckboxInput(attrs={"class":"form-check-input"})
-            
+        labels = {
+            "title": "Título",
+            "description": "Descripción",
+            "important": "Importante"
+        }
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control", "placeholder": "Escribe un título"}),
+            "description": forms.Textarea(attrs={"class": "form-control", "placeholder": "Escribe una descripción"}),
+            "important": forms.CheckboxInput(attrs={"class": "form-check-input"})
         }
