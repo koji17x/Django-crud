b84dce91f7df11a8623c37eb1b835ca5c572a617
koji17x <jandrydavid24@gmail.com>
2024-09-30 00:19:01 -0500

    settings.py update

--- taks/forms.py
+++ taks/forms.py
@@ -5,7 +5,7 @@ from .models import Task
 class taskform(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ["title", "description", "important"]
+        fields = ["title", "description", "due_date", "category", "important"]
         labels = {
             "title": "Título",
             "description": "Descripción",
@@ -16,8 +16,8 @@ class taskform(forms.ModelForm):
         widgets = {
             "title": forms.TextInput(attrs={"class": "form-control", "placeholder": "Escribe un título"}),
             "description": forms.Textarea(attrs={"class": "form-control", "placeholder": "Escribe una descripción"}),
-            "due_date": forms.DateTimeInput(attrs={ "class": "form-control", "placeholder": "Tiempo de vencimiento"}),
-            "status" : forms.TextInput(attrs={"class": "form-control", "placeholder": "Categoria"}),
+            "due_date": forms.DateInput(attrs={ "class": "form-control", "placeholder": "Tiempo de vencimiento", "type": "date"}),
+            "category" : forms.TextInput(attrs={"class": "form-control", "placeholder": "Categoria"}),
             "important": forms.CheckboxInput(attrs={
                 "class": "form-check-input",
                 "style": "border: 1px solid #000; padding: 5px; border-radius: 5px;"
